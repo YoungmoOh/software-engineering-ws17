040d175d705cffb61980939c684980b37fc57d30
Hiyeon <ohyeahbravo@gmail.com>
2017-11-27 17:57:07 +0100

    parameters input exception handling

--- Main.java
+++ Main.java
@@ -1,10 +1,31 @@
 import java.lang.reflect.Method;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) throws Exception
   {
-    String conversion = args[0];
-    String value = args[1];
+	  String conversion;
+	  String value;
+	  
+	  // when two parameters are directly given with main function call
+	  try {
+		    conversion = args[0];
+		    value = args[1];
+	  } // otherwise, get the parameters
+	  catch(ArrayIndexOutOfBoundsException e) {
+		  Scanner scan = new Scanner(System.in);
+		  System.out.print("Enter Converter: ");
+		  conversion = scan.nextLine();
+		  
+		  System.out.print("Enter a value: ");
+		  value = scan.nextLine();
+	  }
+	  
+	  while (!isDouble(value)) {	// the value should be double
+		  Scanner scan2 = new Scanner(System.in);
+		  System.out.print("Please enter a double value: ");
+		  value = scan2.nextLine();
+	  }
 	
 	// dynamically make Instance with the String value
 	Class<?> cls = Class.forName(conversion);
@@ -16,5 +37,14 @@ class Main {
 	m.invoke(obj, value);
 	m = cls.getDeclaredMethod("print");
 	m.invoke(obj);
+  }
+  
+  public static boolean isDouble(String value) {
+	  try {
+		  Double.parseDouble(value);
+	  } catch(NumberFormatException e) {
+		  return false;
+	  }
+	  return true;
   }
 }
